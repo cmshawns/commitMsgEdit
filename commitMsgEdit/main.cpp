a5fbd4db0716630ec130a04b54cc5bb0d42a62ac
Shawn South <shawn@codemastershawn.com>
2014-04-27 02:23:44 -0700

    Loads temp commit message
    App successfully loads the temporary COMMIT_EDITMSG file, but
    is not yet able to save it (compiler errors).

--- commitMsgEdit/main.cpp
+++ commitMsgEdit/main.cpp
@@ -1,12 +1,35 @@
+#include <Core/Core.h>
 #include "commitMsgEdit.h"
+
+using namespace Upp;
 
 CommitMessageEditor::CommitMessageEditor()
 {
 	CtrlLayoutOKCancel(*this, "Commit Message Editor");
 	this->Sizeable(true);
+	
+	ok <<= THISBACK(Save);
+	
+	// load commit message
+	String commitMsg = LoadFile("/home/shawn/dev/commitMsgEdit/.git/COMMIT_EDITMSG");
+	
+	msg.SetData(commitMsg);
 }
 
 GUI_APP_MAIN
 {
 	CommitMessageEditor().Run();
+}
+
+void CommitMessageEditor::Save()
+{
+	if (msg.IsModified())
+	{
+		String newMsg = msg.GetData();
+		
+		if (!SaveFile("/home/shawn/dev/commitMsgEdit/.git/COMMIT_EDITMSG", newMsg))
+		{
+			Exclamation("Message was not saved");
+		}
+	}
 }
